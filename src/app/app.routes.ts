20aa459d5df142c88d7a11c41b1a935203d158cd
Akash Jaiswal <akashjaiswal@Akashs-MacBook-Air.local>
2025-04-26 15:57:35 +0530

    initial3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {path:'home' , component:HomeComponent},
-    {path:'' , component:HomeComponent},
-    {path:'**' , component:HomeComponent},
+    {path:'' , redirectTo:'home',pathMatch:'full'},
+    {path:'**' , redirectTo:'home',pathMatch:'full'},
 ];
